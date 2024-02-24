b1b2179f34bfbf73b40acb4060e63cfddc147e72
suttonet <suttonet@mail.uc.edu>
2024-02-24 14:38:16 -0500

    Changed Home to Thing :)

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,9 @@
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Thing() {
   return (
     <main className={styles.main}>
-      <h1>Jeroober</h1>
-      <p>bottom text</p>
+
     </main>
   );
 }
