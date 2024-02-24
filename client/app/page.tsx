b51eeb82168fa49dc26c22f500f4c47f6394c393
collin-b1 <github.1wjd0@simplelogin.com>
2024-02-24 13:45:04 -0500

    start of client

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Jeroober</h1>
+      <p>bottom text</p>
+    </main>
+  );
+}
