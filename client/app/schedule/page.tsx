dcbaea57cea47308dcad702988428954e2af6ba7
suttonet <suttonet@mail.uc.edu>
2024-02-24 19:57:36 -0500

    more schedule things

--- client/app/schedule/page.tsx
+++ client/app/schedule/page.tsx
@@ -1,26 +1,60 @@
-import { Input, Table, TableBody, TableCell, TableHead, TableRow, Paper, TextField, Button, Box  } from "@mui/material";
+"use client"
+import { Input, TextField, Button, Box, Grid, makeStyles  } from "@mui/material";
 import * as React from 'react';
 import styles from "./page.module.css";
+import { useState } from "react";
 
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
 
 export default function Schedule() {
+  const classes = useStyles()
+  const [classTitle, setClassTitle] = useState('')
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+
+    if (classTitle && start && end){
+      console.log(classTitle, start, end)
+    }
+  }
+
   return (
     <main className={styles.main}>
-      <h1>Add Your Schedule</h1>
+      <h1 className={styles.h1}>Add Your Schedule</h1>
 
-      <Box component="section" sx={{ p: 2, border: '1px solid black'}}>
-      <TextField id="class" label="Class" variant="outlined" />
-      <TextField id="start-time" label="Start Time" variant="outlined" />
-      <TextField id="end-time" label="End Time" variant="outlined" />
-      </Box>
+      <Grid container justifyContent = "center" alignItems='center'  sx={{ p: 4, border: '1px solid black'}}>
+      <TextField onChange={(e) => setClassTitle(e.target.value)}
+       id="class" label="Class" variant="outlined" required />
 
-      <Button variant="contained">Add Class</Button>
+      <TextField onChange={(e) => setStart(e.target.value)}
+       id="start-time" label="Start Time" variant="outlined" required />
 
-      <div className="submit-header">
-        <li>Class</li>
-        <li>Start Time</li> 
-        <li>End Time</li>
-      </div>
+      <TextField onChange={(e) => setEnd(e.target.value)}
+       id="end-time" label="End Time" variant="outlined" required />
+      </Grid>
+
+      <br></br>
+
+      <Button onClick={() => console.log('you clicked me')}
+       variant="contained"  className={styles.button}>Add Class</Button>
+
+      <table>
+        <tbody>
+        <tr>
+          <td className={styles.td}>Class</td>
+          <td className={styles.td}>Start Time</td> 
+          <td className={styles.td}>End Time</td>
+        </tr>
+        </tbody>
+      </table>
     </main>
   );
 
